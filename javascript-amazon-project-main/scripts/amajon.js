ee196a1eb1bbc77ee61ccc27c511304e9fc3ae23
Naveenkumar-R96 <nk946432@gmail.com>
2024-07-19 09:39:29 +0530

    make a cart quantiy interactive

--- javascript-amazon-project-main/scripts/amajon.js
+++ javascript-amazon-project-main/scripts/amajon.js
@@ -80,7 +80,17 @@ document.querySelectorAll('.js-add-to-cart').forEach(function(button){
       });
     }
 
+    let cartQuantity = 0;
     
+    cart.forEach(function(item){
+      cartQuantity+=item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
+    
+    
   });
 });
